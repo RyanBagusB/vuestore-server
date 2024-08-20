04bc1dd4147c2ea90315973016ae915575b982c8
RyanBagusB <134161510+RyanBagusB@users.noreply.github.com>
2024-08-21 02:50:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectDB();
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 
-const PORT = 5000;
+const PORT = 6000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
